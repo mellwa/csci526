5dbeef813d6577ac172d5460fc2f4e28fd935b3b
hffelix <39575629+hffelix@users.noreply.github.com>
2018-06-04 07:22:00 +0800

    healthbar-gamemanager-hf
    several waves of enemies
    health bar added

---
+++ Assets/Scripts/GameManagerBehavior.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManagerBehavior : MonoBehaviour {
+	public Text waveLabel;
+	public GameObject[] nextWaveLabels;
+	public Text goldLabel;
+	private int gold;
+	public bool gameOver = false;
+	private int wave;
+
+	public int Wave
+	{
+		get
+		{
+			return wave;
+		}
+		set
+		{
+			wave = value;
+			if (!gameOver)
+			{
+				for (int i = 0; i < nextWaveLabels.Length; i++)
+				{
+					nextWaveLabels[i].GetComponent<Animator>().SetTrigger("nextWave");
+				}
+			}
+			waveLabel.text = "WAVE: " + (wave + 1);
+		}
+	}
+
+
+	public int Gold {
+		get
+		{ 
+			return gold;
+		}
+		set
+		{
+			gold = value;
+			goldLabel.GetComponent<Text>().text = "GOLD: " + gold;
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		Wave = 0;
+		Gold = 1000;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
